cd5d354289bcfdba6d00924c077a76d789b5d499
Guillermo Posse <gposse@taqtica.com>
2020-02-08 16:40:42 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "gp-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://165t1bk1ub.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_U72r8rVVG",
+      APP_CLIENT_ID: "3hbi6cdoqeiegk8jajulf3o44r",
+      IDENTITY_POOL_ID: "us-east-2:aa562ae3-4ee8-46f9-81a5-ee49e06250d6"
+    }
+};
